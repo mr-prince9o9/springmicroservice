c3d9c5bf8751862828e0b0c63c35f0c414a801b3
mr-prince9o9 <dineshkumarvenkat97@gmail.com>
2025-07-05 00:41:41 +0530

    added injection

--- src/test/java/com/microserviceclient/order_services/OrderTesting.java
+++ src/test/java/com/microserviceclient/order_services/OrderTesting.java
@@ -1,11 +1,17 @@
 package com.microserviceclient.order_services;
 
+import com.microserviceclient.order_services.Repo.OrdersRepo;
+import com.microserviceclient.order_services.Service.OrderService;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 public class OrderTesting {
 @Mock
-  OrderService service;
+OrderService service;
 
+@InjectMocks
+  OrdersRepo repo;
 }
