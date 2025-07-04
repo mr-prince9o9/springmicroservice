0b71cb83dec3818938194bb596d36c3f3d86bf2d
mr-prince9o9 <dineshkumarvenkat97@gmail.com>
2025-07-05 00:33:58 +0530

    commit after modification on test class

--- src/test/java/com/microserviceclient/order_services/OrderTesting.java
+++ src/test/java/com/microserviceclient/order_services/OrderTesting.java
@@ -1,4 +1,10 @@
 package com.microserviceclient.order_services;
 
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
 public class OrderTesting {
+
+
 }
